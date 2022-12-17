21bf18f5f82b2057acf13687493e8b667a44c6f9
ryomaemura <ryomaemura34@gmail.com>
2022-12-17 14:48:34 +0900

    petersen-mine-sweeper

--- Assets/Scenes/NumberManager.cs
+++ Assets/Scenes/NumberManager.cs
@@ -6,6 +6,17 @@ using TMPro;
 
 public class NumberManager : MonoBehaviour
 {
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
+    [SerializeField] Button button3;
+    [SerializeField] Button button4;
+    [SerializeField] Button button5;
+    [SerializeField] Button button6;
+    [SerializeField] Button button7;
+    [SerializeField] Button button8;
+    [SerializeField] Button button9;
+    [SerializeField] Button button10;
+    Button[] buttons;
     [SerializeField] TextMeshProUGUI buttonText1;
     [SerializeField] TextMeshProUGUI buttonText2;
     [SerializeField] TextMeshProUGUI buttonText3;
@@ -16,93 +27,81 @@ public class NumberManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI buttonText8;
     [SerializeField] TextMeshProUGUI buttonText9;
     [SerializeField] TextMeshProUGUI buttonText10;
-    [SerializeField] TextMeshProUGUI buttonText11;
-    [SerializeField] TextMeshProUGUI buttonText12;
-    [SerializeField] TextMeshProUGUI buttonText13;
-    [SerializeField] TextMeshProUGUI buttonText14;
-    [SerializeField] TextMeshProUGUI buttonText15;
-    [SerializeField] TextMeshProUGUI scoreLabel;
     TextMeshProUGUI[] buttonTexts;
-    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0};
+    [SerializeField] TextMeshProUGUI bombText;
+    [SerializeField] TextMeshProUGUI gameOverText;
+    int[] numbers = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     int[, ] relasionshipNumbers = {
-    //   1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
-        {0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+    //   1  2  3  4  5  6  7  8  9  10
+        {0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+        {1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+        {1, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+        {0, 0, 1, 0, 0, 1, 0, 1, 0, 0},
 
-        {0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
-        {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0},
-        {0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0},
-        {0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0},
-        {0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1},
-        
-        {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+        {0, 0, 0, 1, 1, 0, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 1, 0, 0, 1, 0},
+        {0, 1, 0, 0, 1, 0, 0, 0, 0, 1},
+        {0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+        {0, 0, 0, 1, 0, 0, 0, 1, 1, 0},
     };
-    int temp = 0;
-    int randomNumber1 = 0;
-    int randomNumber2 = 0;
-    int scoreNumber = 0;
+    int bombNumber = 2;
+    int randomNumber = 0;
+    int relasionshipBombsNumber = 0;
 
     // Start is called before the first frame update
     void Start() {
-        buttonTexts = new TextMeshProUGUI[] {buttonText1, buttonText2, buttonText3, buttonText4, buttonText5, buttonText6, buttonText7, buttonText8, buttonText9, buttonText10, buttonText11, buttonText12, buttonText13, buttonText14, buttonText15};
+        buttons = new Button[] {button1, button2, button3, button4, button5, button6, button7, button8, button9, button10};
+        buttonTexts = new TextMeshProUGUI[] {buttonText1, buttonText2, buttonText3, buttonText4, buttonText5, buttonText6, buttonText7, buttonText8, buttonText9, buttonText10};
 
-        shuffleNumbers();
-        setNumbers();
-        scoreReload();
+        resetBomb();
     }
 
     // Update is called once per frame
     void Update() {
     }
 
-    public void clickButton(int number) {
+    public void resetBomb() {
         for (int i = 0; i < numbers.Length; i++) {
-            if (relasionshipNumbers[number - 1, i] == 1 && numbers[i] == 0) {
-                temp = numbers[number - 1];
-                numbers[number - 1] = numbers[i];
-                numbers[i] = temp;
-
-                buttonTexts[number - 1].text = "";
-                buttonTexts[i].text = numbers[i].ToString();
-
-                scoreNumber = scoreNumber + 1;
-                scoreReload();
-            }
+            numbers[i] = 0;
+            buttons[i].GetComponent<Image>().color = new Color32(100, 0, 200, 255);
+            buttonTexts[i].text = "";
         }
-    }
 
-    public void setNumbers() {
-        for (int i = 0; i < numbers.Length; i++) {
-            if (numbers[i] != 0) {
-                buttonTexts[i].text = numbers[i].ToString();
+        for (int i = 0; i < bombNumber; i++) {
+            // output 0 ~ numbers.length - 1
+            randomNumber = UnityEngine.Random.Range(0, numbers.Length);
+
+            if (numbers[randomNumber] != 1) {
+                numbers[randomNumber] = 1;
             } else {
-                buttonTexts[i].text = "";
+                i--;
             }
         }
+
+        bombText.text = "Bomb:" + bombNumber.ToString();
+        gameOverText.text = "";
     }
 
-    public void shuffleNumbers() {
-        for (int i = 0; i < 30; i++) {
-            randomNumber1 = UnityEngine.Random.Range(0, numbers.Length);
-            randomNumber2 = UnityEngine.Random.Range(0, numbers.Length);
+    public void clickNumberButton(int buttonNumber) {
+        relasionshipBombsNumber = 0;
 
-            temp = numbers[randomNumber1];
-            numbers[randomNumber1] = numbers[randomNumber2];
-            numbers[randomNumber2] = temp;
+        if (numbers[buttonNumber] == 0) {
+            // not bomb
+            buttons[buttonNumber].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+            for (int i = 0; i < numbers.Length; i++) {
+                if (relasionshipNumbers[buttonNumber, i] == 1 && numbers[i] == 1) {
+                    relasionshipBombsNumber++;
+                }
+            }
+
+            buttonTexts[buttonNumber].text = relasionshipBombsNumber.ToString();
+        } else {
+            // bomb
+            buttons[buttonNumber].GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+
+            gameOverText.text = "Game Over";
         }
-
-        scoreNumber = 0;
-        scoreReload();
-    }
-
-    public void scoreReload() {
-        scoreLabel.text = "Move:" + scoreNumber.ToString();
     }
 }
